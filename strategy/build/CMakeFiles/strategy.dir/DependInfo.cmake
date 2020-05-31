14f02594d874609df50b7977a1bf65f300f4bf19
ZhangBo <495203317@qq.com>
2020-05-31 19:15:06 +0800

    first commit

---
+++ strategy/build/CMakeFiles/strategy.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "G:/VSCode_Workspace/Design_Patterns/strategy/src/main.cpp" "G:/VSCode_Workspace/Design_Patterns/strategy/build/CMakeFiles/strategy.dir/src/main.cpp.obj"
+  "G:/VSCode_Workspace/Design_Patterns/strategy/src/strategy.cpp" "G:/VSCode_Workspace/Design_Patterns/strategy/build/CMakeFiles/strategy.dir/src/strategy.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../$(PROJECT_SOURCE_DIR)"
+  "C:/Develop/opencv/build/x64/MinGW/install/include"
+  "C:/Develop/opencv/build/x64/MinGW/install/include/opencv2"
+  "C:/MinGW/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
